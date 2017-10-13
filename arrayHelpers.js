a0a0cbe6128aefef8310ecaec95a2488c407233a
Mario Aburto <marioaburto10@gmail.com>
2017-10-12 23:56:44 -0400

    intro to map() helper function

--- arrayHelpers.js
+++ arrayHelpers.js
@@ -97,3 +97,21 @@
 	console.log(areas); // [300, 1800, 1728]
 
 
+
+// map() helper method
+
+	var numbers = [1, 2, 3];
+	var doubledNumbers = [];
+
+	for (var i = 0; i < numbers.length; i++) {
+		doubledNumbers.push(numbers[i] * 2);
+	}
+
+	console.log(doubledNumbers); // [2, 4, 6]
+
+	// refactored code using map() helper method
+	 var doubled = numbers.map(function(number){
+	 	return number * 2;
+	 });
+
+	 console.log(doubled); // [2, 4, 6]
