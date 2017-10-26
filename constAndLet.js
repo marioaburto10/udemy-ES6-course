5a3dc9db3eef5a1907d5484523cf99b6577e6e73
Mario Aburto <marioaburto10@gmail.com>
2017-10-25 22:30:22 -0400

    added more of an explanation of the differences between var and let

--- constAndLet.js
+++ constAndLet.js
@@ -7,6 +7,11 @@
 // const is for variables that will never be changed once declared 
 // let is for variables that can be changed after they are declared
 
+// let allows you to declare variables that are limited in scope to the block, statement, or expression on which it is used. This is unlike the var keyword, which defines a variable globally, or locally to an entire function regardless of block scope.
+// var is scoped to the nearest function block and let is scoped to the nearest enclosing block, which can be smaller than a function block. Both are global if outside any block.
+// Assuming strict mode, var will let you re-declare the same variable in the same scope. On the other hand, let will not
+
+
 const name = 'Jane';
 let title = 'Software Engineer';
 let hourlyWage = 45;
@@ -33,7 +38,7 @@ for (var i = 0; i < statuses.length; i++) {
   }
 }
 
-// statuses will not be changed but message, currentCode, and i will be changed throughout the code
+// statuses will not be changed but message, currentCode, and i will be changed throughout the code 
 const statuses = [ 
   { code: 'OK', response: 'Request successful' },
   { code: 'FAILED', response: 'There was an error with your request' },
