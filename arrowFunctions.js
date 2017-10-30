c8df82a7e25d467a312ce19bd2f27a3b1c4ea976
Mario Aburto <marioaburto10@gmail.com>
2017-10-29 23:13:43 -0400

    added examples and more fat arrow function syntax

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,3 +1,6 @@
+// fat arrow functions let us write cleaner code
+// they allow us to ommit unnecessary characters
+
 // how a regular function is written
 const add = function(a, b) {
 	return a + b;
@@ -19,3 +22,34 @@ console.log(add2(3,4)); // 7
 const add3 = (a, b) => a + b;
 
 console.log(add3(3,4)); // 7
+
+
+// Whenever there is a single argument, the parentheses can be omitted
+const double = number => 2 * number;
+
+console.log(double(5)); // 10
+
+// If there are no arguments at all, parends are still needed
+const returnTwo = () => 2;
+
+console.log(returnTwo()); // 2
+
+
+// Ex. 1 - using fat arrow functions with array helpers
+// ES5 way
+const numbers = [1, 2, 3];
+
+var doubledArray = numbers.map(function(number) {
+	return 2 * number;
+});
+
+console.log(doubledArray); // [ 2, 4, 6 ]
+
+// ES6, using fat arrows now
+// we ommit curly braackets, the word return, parentheses around the arguement, and no semicolon after the function. There is a semicolon to finish the map function
+var doubledArray2 = numbers.map(number => 2 * number);
+
+console.log(doubledArray2); // [ 2, 4, 6 ]
+
+
+
