3a65af3ad274e644b5c6cc35834a2da29ab1c3d0
Mario Aburto <marioaburto10@gmail.com>
2017-10-31 20:18:52 -0400

    added examples for enhanced object literals

--- enhancedObjectLiterals.js
+++ enhancedObjectLiterals.js
@@ -41,3 +41,61 @@ function createBookShop2(inventory) {
 const bookShop2 = createBookShop2(inventory);
 console.log(bookShop2.inventoryValue()); // 25
 console.log(bookShop2.priceForTitle('Harry Potter')); // 10
+
+
+// Ex. 1 - Multiple Properties with Enhanced Notation
+// Refactor to use enhanced literal notation
+const red = '#ff0000';
+const blue = '#0000ff';
+
+let COLORS = { red: red, blue: blue };
+
+// refactored 
+COLORS = {red, blue };
+console.log(COLORS); // { red: '#ff0000', blue: '#0000ff' }
+
+
+// Ex. 2 - Literals in Functions
+// Refactor to use enhanced literal notation
+let canvasDimensions = function(width, initialHeight) {
+  const height = initialHeight * 9 /16;
+  return { 
+    width: width, 
+    height: height 
+  };
+}
+
+// refactored
+canvasDimensions = function(width, initialHeight) {
+  const height = initialHeight * 9 /16;
+  return { 
+    width, 
+    height 
+  };
+}
+
+
+// Ex. 3 - Refactor to use enhanced literal notation
+// Refactor to use enhanced literal notation
+const color = 'red';
+
+let Car = {
+  color: color,
+  drive: function() {
+    return 'Vroom!';
+  },
+  getColor: function() {
+    return this.color;
+  }
+};
+
+// refactored
+Car = {
+  color,
+  drive() {
+    return 'Vroom!';
+  },
+  getColor() {
+    return this.color;
+  }
+};
