6a925c1d43a766b0b69cc5131a8943ae8d3d3350
Mario Aburto <marioaburto10@gmail.com>
2017-10-31 22:01:29 -0400

    added examples of default function arguments

--- defaultFunctionArguments.js
+++ defaultFunctionArguments.js
@@ -21,3 +21,48 @@ console.log(makeAjaxRequest2('google.com')); // GET
 console.log(makeAjaxRequest2('google.com', 'POST')); // POST
 // When passing in null, method will not get reassigned to 'GET'
 console.log(makeAjaxRequest2('google.com', null)); // null
+
+
+// Ex. 1 - Using Default Arguments
+// Refactor the following code to use default function arguments.
+	function sum(a, b) {
+	  if (a === undefined) {
+	    a = 0; 
+	  }
+	  
+	  if (b === undefined) {
+	    b = 0; 
+	  }
+	  
+	  return a + b;
+	}
+
+	// Refactored
+	function sum2(a = 0, b = 0) {
+	 
+	  return a + b;
+	}
+
+	console.log(sum2()); // 0
+
+
+// Ex. 2 - Dumping Unused Code
+// Refactor the following code to use default function arguments.  Be sure to remove any unused code after you refactor it.
+	function addOffset(style) {
+	  if (!style) {
+	    style = {}; 
+	  }
+	  
+	  style.offset = '10px';
+	  
+	  return style;
+	}
+
+	// Refactored
+	function addOffset2(style = {}) {
+	  style.offset = '10px';
+	  
+	  return style;
+	}
+
+	console.log(addOffset2()); // { offset: '10px' }
